53621128de5a9e8a6138e12f5b93ee9687e4ec77
Mannn21 <aimannurrofi21@gmail.com>
2023-07-13 21:07:02 +0700

    change project

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import Hero from "../../components/Hero"
+
+const Home = () => {
+    return (
+        <div className="w-screen h-screen">
+            <Hero />
+        </div>
+    )
+}
+
+export default Home
